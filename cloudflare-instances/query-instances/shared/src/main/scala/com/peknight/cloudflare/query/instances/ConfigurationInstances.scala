daeafdb7865ccf252647b4c2198c4b0bbf030099
peknight <JKpeknight@gmail.com>
2024-04-20 22:04:35 +0800

    update

--- cloudflare-instances/query-instances/shared/src/main/scala/com/peknight/cloudflare/query/instances/ConfigurationInstances.scala
+++ cloudflare-instances/query-instances/shared/src/main/scala/com/peknight/cloudflare/query/instances/ConfigurationInstances.scala
@@ -3,6 +3,6 @@ package com.peknight.cloudflare.query.instances
 import com.peknight.query.configuration.Configuration
 
 trait ConfigurationInstances:
-  given Configuration = Configuration(defaultKeys = List("equal", "exact"))
+  given Configuration = Configuration(defaultKeys = List("equal", "exact"), flagKeys = List("absent", "present"))
 end ConfigurationInstances
 object ConfigurationInstances extends ConfigurationInstances
