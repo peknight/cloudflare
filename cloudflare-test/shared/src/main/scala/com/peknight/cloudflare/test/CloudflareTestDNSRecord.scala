91e502ee3a63ad139a0b2a95a00cb08d1b427ef5
peknight <JKpeknight@gmail.com>
2024-04-23 22:46:58 +0800

    update

--- cloudflare-test/shared/src/main/scala/com/peknight/cloudflare/test/CloudflareTestDNSRecord.scala
+++
@@ -1,7 +0,0 @@
-package com.peknight.cloudflare.test
-
-import com.peknight.cloudflare.dns.record.DNSRecordId
-
-trait CloudflareTestDNSRecord:
-  def dnsRecordId: DNSRecordId
-end CloudflareTestDNSRecord
