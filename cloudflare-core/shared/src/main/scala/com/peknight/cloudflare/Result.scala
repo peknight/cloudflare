e0eb98b3f8885cb76963e4a2ebf9cffcd1fcd498
peknight <JKpeknight@gmail.com>
2025-04-11 18:12:33 +0800

    update

--- cloudflare-core/shared/src/main/scala/com/peknight/cloudflare/Result.scala
+++ cloudflare-core/shared/src/main/scala/com/peknight/cloudflare/Result.scala
@@ -1,7 +1,6 @@
 package com.peknight.cloudflare
 
 import cats.data.Ior
-import com.peknight.api.pagination.Pagination
 
 case class Result[A](
                       result: Option[A],
@@ -9,12 +8,12 @@ case class Result[A](
                       resultInfo: Option[ResultInfo],
                       errors: List[Error],
                       msgs: List[Message]
-                    ) extends com.peknight.api.Result[A]:
+                    ) extends com.peknight.api.Result[ResultInfo, A]:
   def ior: com.peknight.error.Error Ior A =
     (result, errors, msgs) match
       case (Some(res), Nil, Nil) => Ior.right(res)
       case (Some(res), errs, msgs) => Ior.both(com.peknight.error.Error(errs ::: msgs), res)
       case (None, errs, msgs) => Ior.left(com.peknight.error.Error(errs ::: msgs))
   override def data: Option[A] = result
-  override def pagination: Option[Pagination] = resultInfo
+  override def pagination: Option[ResultInfo] = resultInfo
 end Result
