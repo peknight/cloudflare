88f78c73628ebbce40a98538e48bd474d25442ae
peknight <JKpeknight@gmail.com>
2025-05-21 09:36:07 +0800

    update

--- cloudflare-zone/instances/query/shared/src/main/scala/com/peknight/cloudflare/zone/query/instances/query/ListZonesQueryInstances.scala
+++ cloudflare-zone/instances/query/shared/src/main/scala/com/peknight/cloudflare/zone/query/instances/query/ListZonesQueryInstances.scala
@@ -2,12 +2,11 @@ package com.peknight.cloudflare.zone.query.instances.query
 
 import cats.Monad
 import com.peknight.cloudflare.zone.codec.instances.query.listZonesQuery.codecListZonesQuery
-import com.peknight.cloudflare.zone.query.{Account, ListZonesQuery}
+import com.peknight.cloudflare.zone.query.ListZonesQuery
 import com.peknight.codec.Codec
 import com.peknight.codec.config.CodecConfig
 import com.peknight.codec.cursor.Cursor
 import com.peknight.query.Query
-import com.peknight.query.codec.id.Encoder
 
 trait ListZonesQueryInstances:
   given queryCodecListZonesQuery[F[_]](using CodecConfig, Monad[F]): Codec[F, Query, Cursor[Query], ListZonesQuery] =
