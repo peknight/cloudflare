87b5ef8c8481c036f9833443ba012f558d64d928
peknight <JKpeknight@gmail.com>
2026-02-06 18:07:58 +0800

    update

--- cloudflare-zone/config/shared/src/main/scala/com/peknight/cloudflare/zone/config/CloudflareZoneConfig.scala
+++ cloudflare-zone/config/shared/src/main/scala/com/peknight/cloudflare/zone/config/CloudflareZoneConfig.scala
@@ -2,7 +2,7 @@ package com.peknight.cloudflare.zone.config
 
 import cats.effect.std.Env
 import cats.{Monad, MonadError, Show}
-import com.peknight.auth.Token
+import com.peknight.auth.Token.Bearer
 import com.peknight.cloudflare.zone.ZoneId
 import com.peknight.codec.config.{CodecConfig, given}
 import com.peknight.codec.cursor.Cursor
@@ -11,16 +11,13 @@ import com.peknight.codec.reader.Key
 import com.peknight.codec.sum.{NullType, ObjectType, StringType}
 import com.peknight.codec.{Codec, Decoder}
 
-case class CloudflareZoneConfig(token: Token, zoneId: ZoneId)
+case class CloudflareZoneConfig(token: Bearer, zoneId: ZoneId)
 object CloudflareZoneConfig:
   given codecCloudflareZoneConfig[F[_], S](using CodecConfig)(using Monad[F], ObjectType[S], NullType[S], StringType[S],
                                                               Show[S]): Codec[F, S, Cursor[S], CloudflareZoneConfig] =
-    given Codec[F, String, String, Token] = Codec.map[F, String, String, Token](_.token)(Token.Bearer.apply)
-    given Codec[F, S, Cursor[S], Token] = Codec.codecS[F, S, Token]
     Codec.derived[F, S, CloudflareZoneConfig]
 
   given keyDecodeCloudflareZoneConfig[F[_]](using MonadError[F, Throwable], Env[F]): Decoder[F, Key, CloudflareZoneConfig] =
-    given Decoder[F, String, Token] = Decoder[F, String, String].map(Token.Bearer.apply)
     Decoder.derivedByKey[F, CloudflareZoneConfig]
   given Show[CloudflareZoneConfig] = Show.show[CloudflareZoneConfig](config => s"CloudflareZoneConfig(${config.zoneId})")
 end CloudflareZoneConfig
